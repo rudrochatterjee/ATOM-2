f96b9087c0865b4d2b03625d588b4f41bf502e89
rudropro <58634478+rudropro@users.noreply.github.com>
2020-12-24 15:20:16 +0530

    music

--- Index.js
+++ Index.js
@@ -1,45 +1,74 @@
+const { executionAsyncResource } = require('async_hooks');
 const Discord = require('discord.js');
+const { measureMemory } = require('vm');
+const ytdl = require('ytdl-core');
+const fs = require('fs')
+
+const { YTSearcher } = require('ytsearcher');
+
+const searcher = new YTSearcher({
+    key: "AIzaSyDsGp7lYJOOd5EI2Rw42QYM_K7SZp1Iph4",
+    revealed: true
+});
+
 const client = new Discord.Client();
-const config = require("./config.json");
-const api = require("imageapi.js")
-const ytdl = require("ytdl-core")
-client.on('ready', message =>{
-    console.log('music bot is on')
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+
+fs.readdir("./commands/", (e, f) => {
+    if(e) return console.error(e);
+    f.forEach(file => {
+        if(!file.endsWith(".js")) return
+        console.log(`${file} has been loaded`)
+        let cmd = require(`./commands/${file}`);
+        let cmdName = cmd.config.name;
+        client.commands.set(cmdName, cmd)
+        cmd.config.aliases.forEach(alias => {
+            client.aliases.set(alias, cmdName);
+        })
+    })
 })
-const prefix = config.prefix;
-client.on('message', async message =>{
-    if(message.author.bot) return
-    if(!message.content.startsWith(prefix)) return
 
-    const args = message.content.substring(config.prefix.length).split(" ")
 
-    if(message.content.substring(`${prefix}play`)){
-        const vc = message.member.voice.channel
-        if(!vc) return message.channel.send('you need to join a vc to play song')
-        const perm = vc.permissionsFor(message.client.user)
-        if(!perm.has("CONNECT")) return message.channel.send("i dont have permission to connect")
-        if(!perm.has('SPEAK')) return message.channel.send("i dont have permission to speak")
 
-        try{
-           var connection = await vc.join()
-        } catch (error) {
-           console.log(` There was an error to join the channel : ${error}`)
-           return message.channel.send(`There was an error to join the channel : ${error}`)
-        }
+const queue = new Map();
 
-        const dispatcher = connection.play(ytdl(args[1]))
-        .on('finish', () => {
-            vc.leave()
-        })
-        .on('error', error => {
-            console.log(error)
-        })
-        dispatcher.setVolumeLogarithmic(5 / 5)
-    }else if(message.content.startsWith(`${prefix}stop`)){
-        if(!message.member.voice.channel) return message.channel.send('you need to be in a vc to stop the music') 
-        message.member.voice.channel.leave()
-        return undefined
+client.on("ready", () => {
+    console.log("I am online!")
+})
+
+
+client.on("guildMemberAdd", member => {
+    if (member.user.bot){
+        let role = member.guild.roles.cache.find(role => role.name === "Dumbass")
+        return member.roles.add(role)
     }
+
+    let role = member.guild.roles.cache.find(role => role.name === "Epic Gamers")
+    member.roles.add(role)
 })
 
-client.login(config.token)
+
+client.on("message", async(message) => {
+    const prefix = '!';
+
+    if(!message.content.startsWith(prefix)) return
+    
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+    const command = args.shift().toLowerCase();
+
+    const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
+
+    if(!cmd) return
+
+    try {
+        cmd.run(client, message, args, queue, searcher);
+    }catch (err){
+        return console.error(err)
+    }
+        
+})
+
+const token = "NzkxMjc5NzM2NTExNzkxMTY1.X-M2rg.V08BoWY2-VmGdqvCDHlIfYBv3yM"
+
+client.login(token)
